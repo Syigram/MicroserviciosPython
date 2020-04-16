2d557cf32b25b7cb5f2081fe3c5a514567863116
Alejandro Gonzalez <sygramdev@gmail.com>
2020-04-16 12:25:21 -0600

    add: Flask Web Application

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 import sqlite3
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 
 
 APP = Flask(__name__)
@@ -24,10 +25,65 @@ class User(DB.Model):
     date_created = DB.Column(DB.DateTime, default=datetime.now)
 
 
+@APP.route('/users', methods=['GET'])
+def view_users():
+    users = User.query.order_by(User.name).all()
+    users_list = []
+    for user in users:
+        info = {
+            'name': user.name,
+            'email': user.email,
+            'region': user.region,
+        }
+        users_list.append(info)
+    return jsonify(users_list)
+
+
+@APP.route('/register', methods=['GET'])
+def register_user():
+    try:
+        name = request.args.get('name')
+        region = request.args.get('region')
+        email = request.args.get('email')
+        new_user = User(name=name, region=region, email=email)
+        DB.session.add(new_user)
+        DB.session.commit()
+        return (success_response('Registered user!'), 200)
+    except exc.IntegrityError:
+        missing_args = []
+        if name is None:
+            missing_args.append('name')
+        if region is None:
+            missing_args.append('region')
+        if email is None:
+            missing_args.append('email')
+        DB.session.rollback()
+        return (error_response(missing_args), 400)
+
 @APP.route('/')
 @APP.route('/index')
 def hello_world():
     return 'Hello World!'
+
+
+def success_response(message):
+    return json_response(200, 'OK', message)
+
+def error_response(args_list):
+    msg_list = []
+    message = ''
+    for arg in args_list:
+        message = f'Missing parameter {arg}'
+        msg_list.append(message)
+    return json_response(400, 'Bad request', msg_list)
+
+
+def json_response(code, status, message):
+    return {
+        'Status': code,
+        'StatusText': status,
+        'Message': message,
+    }
 
 
 def main():
